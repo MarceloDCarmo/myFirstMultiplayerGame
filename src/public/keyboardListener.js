fb8a7cc46fe09c91dabeeb70147c54f5543e6bab
Marcelo Dias <marcelodkarmo@gmail.com>
2022-10-05 16:30:23 -0300

    Complete game based on Filipe Deschamps' playlist 'Primeiro Jogo Multiplayer'

---
+++ src/public/keyboardListener.js
@@ -0,0 +1,40 @@
+export default function createKeyboardListener(document) {
+    const state = {
+        observers: [],
+        playerId: null
+    }
+
+    function subscribe(observerFunction) {
+        state.observers.push(observerFunction)
+    }
+
+    function notifyAll(command) {
+
+        for (const observerFunction of state.observers) {
+            observerFunction(command)
+        }
+    }
+
+    function registerPlayerId(playerId) {
+        state.playerId = playerId
+    }
+
+    document.addEventListener('keydown', handleKeydown)
+
+    function handleKeydown(event) {
+        const keyPressed = event.key
+
+        const command = {
+            type: 'move-player',
+            playerId: state.playerId,
+            keyPressed
+        }
+
+        notifyAll(command)
+    }
+
+    return {
+        subscribe,
+        registerPlayerId
+    }
+}
---
+++ src/server.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+import createGame from './public/game.js'
+
+const app = express()
+const server = http.createServer(app)
+
+const sockets = new Server(server)
+
+app.use(express.static('src/public'))
+
+const game = createGame()
+
+game.start()
+
+game.subscribe((command) => {
+    sockets.emit(command.type, command)
+})
+
+sockets.on('connection', (socket) => {
+    const playerId = socket.id
+    console.log(`> Player connected on Server with id: ${playerId}`)
+
+    game.addPlayer({ playerId: playerId })
+
+    socket.emit('setup', game.state)
+
+    socket.on('disconnect', () => {
+        game.removePlayer({ playerId })
+        console.log(`Player ${playerId} disconnected`)
+    })
+
+    socket.on('move-player', (command) => {
+        command.playerId = playerId
+        command.type = 'move-player'
+
+        game.movePlayer(command)
+    })
+})
+
+server.listen(3000, () => console.log('Server listen on port 3000'))
